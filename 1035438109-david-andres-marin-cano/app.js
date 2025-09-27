6481d82e312db1372e1c51b5a398be59be721e32
david98m <damc91@gmail.com>
2025-09-27 13:36:52 -0500

    Fase 2: Implementadas funciones para ventas y reposición de stock

--- 1035438109-david-andres-marin-cano/app.js
+++ 1035438109-david-andres-marin-cano/app.js
@@ -7,4 +7,24 @@ function mostrarInventario() {
     console.log(`Producto: ${nombreProducto}, Stock: ${stockProducto} unidades, Ventas: ${ventasTotales} unidades.`);
 }
 
-mostrarInventario();
+
+const registrarVenta = function(cantidadVendida) {
+    if (cantidadVendida <= stockProducto) {
+        stockProducto -= cantidadVendida;
+        ventasTotales += cantidadVendida;
+        console.log("Venta registrada con éxito.");
+    } else {
+        console.log("Stock insuficiente para esta venta.");
+    }
+};
+
+const reponerStock = (cantidadRepuesta) => {
+    stockProducto += cantidadRepuesta;
+    console.log(`Se añadieron ${cantidadRepuesta} unidades. Nuevo stock: ${stockProducto}.`);
+};
+
+
+registrarVenta(10);   // Venta válida
+registrarVenta(50);   // Venta que excede el stock
+reponerStock(20);     // Reposición
+mostrarInventario();  // Estado final
